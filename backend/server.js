1f2b1a9c6661fe87619960f195fe782f37256b93
RAHGUL MS <160340095+msrahgul@users.noreply.github.com>
2025-04-14 10:55:10 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 const db = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "Rahgul@2006",
+    password: "***********",
     database: "car_rental_system",
 });
 
